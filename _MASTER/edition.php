0763b912b7aebf4e2ee576f274353664b1a3af74
florentlagrangeipx <contact@florentlagrange.eu>
2015-10-17 11:45:10 +0200

    Introduction_MASTER
    Introduction_MASTER

---
+++ _MASTER/edition.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+
+<meta name="DESCRIPTION" content="IPX / Internet eXchange Point"/>
+<meta name="description" content="IPX / Internet eXchange Point" />
+<meta name="keyword" content="IPX, Internet eXchange Point, Ecole, Edition, Logiciel, Plateforme, Echange, Partage, Numérique, Art, Lyon, Rhone-Alpes  "/>
+<meta name="keywords" content="IPX, Internet eXchange Point, Ecole, Edition, Logiciel, Plateforme, Echange, Partage, Numérique, Art, Lyon, Rhone-Alpes"/>
+<meta name="SUBJECT" content="IPX, Internet eXchange Point, Ecole, Edition, Logiciel, Plateforme, Echange, Partage, Numérique, Art, Lyon, Rhone-Alpes "/>
+<meta name="RATING" content="IPX, Internet eXchange Point, Ecole, Edition, Logiciel, Plateforme, Echange, Partage, Numérique, Art, Lyon, Rhone-Alpes"/>
+
+<meta name="EDITORS" content="ipx-lab.com"/>
+
+<meta name="REVISIT-AFTER" content="10 DAYS"/>
+<meta name="ROBOTS" content="All"/>
+
+
+
+
+<title>EDITION / Internet eXchange Point</title>
+
+
+<!-- 
+
+.....__              
+	|__|________  ___
+	|  \____ \  \/  /
+	|  |  |_> >    < 
+	|__|   __/__/\_ \
+	   |__|        \/
+
+ -->
+
+<link href="css/ipx.css" rel="stylesheet" type="text/css" />
+
+
+
+<style type="text/css">
+body {
+
+	color: #333;
+}
+
+
+a:link {
+	color: #FFF;
+	text-decoration: none;
+}
+a:visited {
+	color: #FFF;
+	text-decoration: none;
+}
+a:hover {
+	color: #FFF;
+	text-decoration: none;
+}
+a:active {
+	text-decoration: none;
+}
+
+
+.Bouton_IPX {
+	background-color:#EF7273;
+	-moz-border-radius:42px;
+	-webkit-border-radius:42px;
+	border-radius:42px;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Trebuchet MS;
+	font-size:14px;
+	padding:13px 10px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #c9c5c9;
+}
+.Bouton_IPX:hover {
+	background-color:#dedade;
+}
+.Bouton_IPX:active {
+	position:relative;
+	top:1px;
+}
+.Bouton_Modules {
+	background-color:#EF7273;
+	-moz-border-radius:42px;
+	-webkit-border-radius:42px;
+	border-radius:42px;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Trebuchet MS;
+	font-size:12px;
+	padding:18px 6px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #c9c5c9;
+}
+.Bouton_Modules:hover {
+	background-color:#dedade;
+}
+.Bouton_Modules:active {
+	position:relative;
+	top:1px;
+}
+
+.lienedition{
+	color:#EF7273;
+}
+
+
+.container .content table {
+	text-align: center;
+}
+
+
+
+
+
+</style>
+</head>
+
+<body>
+
+<div class="container">
+  <div class="sidebar1"><br /> <br /> <br /> 
+    <table width="100%" border="0" cellspacing="0">
+  <tr>
+    <th scope="col"><a class="Bouton_IPX"  href="../index.html">IPX</a></th>
+    <th scope="col"><a href="contact.php"><img src="img/logo.png" style="opacity: 0.4;" width="40%" ></a></th>
+  </tr>
+</table>
+
+ </div>
+ 
+
+
+
+
+<div class="content">
+
+
+
+
+<!-- PARTIE I - EDUCATION -->
+
+
+<!-- école n°1-->
+
+<p><br />
+  </p>
+   <div class="titre"></div>
+   <p><br />
+    <!--FIN //////// PARTIE I - EDUCATION --></p>
+   <div class="titre"><span class="Titre_partie"></span></div>
+   <p>&nbsp;</p>
+   <div class="titre">
+     <!--  		@page { margin: 2cm }  		P { margin-bottom: 0.21cm }  	-->
+     <BODY DIR="LTR">
+     <p align="center" style="font-size:2.5em; color:#EF7273;">&Eacute;DITION<br />
+     </p>
+<p align="center"><i>Internet eXchange Point</i></p>
+     <p align="center"><br /><br /><br /><br />
+     </p>
+    
+    
+     <p><em class="sous_titre_edition">Présentation</em><br />
+ </p>
+<p>Afin d'adapter son offre aux nouveaux besoins ergonomiques lié à l'usage du réseau, le collectif IPX oriente sa production du côté de la publication papier et numérique au sein du module ÉDITION. <br /><br />Pour cela il prend en compte l'histoire et l'émergence des nouveaux supports de lecture - ordinateurs, smartphones, tablettes, liseuses – entre lesquels les contenus circulent, afin de proposer des créations éditoriales originales et sur mesure.</p>
+     <p><br />
+     </p>
+    
+    
+     <p><em class="sous_titre_edition">Composition de la recherche</em><br />
+ </p>
+<p>Ce module est mis en place à partir d'un savoir (et savoir-faire) évolutif, dynamique et partagé. Il se situe au croisement de différentes problématiques de recherche contemporaines - entre accessibilité de l'ouvrage, enjeux économiques modernes et nouveaux modes de création et de distribution – à partir desquels les membres du collectif tendent à développer une chaîne de production éditoriale inédite.</p>
+ <p><br />
+     </p>
+    
+  
+     <p><em class="sous_titre_edition">Objectifs</em><br />
+ </p>
+<p>EDITION réunit des services d'expertise tel qu' un accompagnement personnalisé pour la réalisation de livres papier et électroniques ainsi qu'un accès à « l'observatoire » des pratiques éditoriales, une base de ressources libre, gratuite et accessible à tous.</p>
+<p>
+Les pratiques personnelles d'artistes et de graphistes des membres du collectif, viennent également alimenter le contenu des différentes prestations proposé au sein ce module. De la création d'un concept de collection, au dévellopement d'une charte graphique originale, à un travail de mise en page ou ou de typographie précis. etc.</p>
+<p>
+Afin de toujours privilégier le contact et d'être au plus prés des besoins de ses clients, la plateforme IPX génère des devis comprenant des temps d'échanges et de discussions que le collectif considère comme indispensable dans l'élaboration de chaque projet. Ainsi IPX prend en charge la conception d'objets éditoriaux aux contenus hétérogènes, qu'il s'agisse d'un livre d'artiste, d'un manuel de ressources pédagogique, d'un support de communication ou d'un livre de recettes. </p>
+ <p><br />
+     </p>
+    
+ <p><em class="sous_titre_edition">Perspectives</em><br />
+ </p>
+<p>La plateforme Internet eXchange Point est à envisager comme un relais, un nœud dans le réseau de distribution globale des informations. En faisant un pas de côté vis-à-vis de modèles éditoriaux traditionnels, en questionnant le statut de l'auteur, tout en lançant sa propre ligne éditoriale le collectif IPX tendra à interroger de façon générale le champ de diffusion des savoirs.
+<br /><br /><br />
+</p>
+   </div>
+   <p>&nbsp;</p>
+  <table width="100%" border="0">
+     <tr>
+       <td><span class="titre"><span class="Titre_partie"><a class="Bouton_Modules" href="ecole.php">&nbsp;école&nbsp;</a></span></span></td>
+       <td><span class="titre"><span class="Titre_partie"><a class="Bouton_Modules"  href="logiciel.php">logiciel</a></span></span></td>
+       <td><span class="titre"><span class="Titre_partie"><a class="Bouton_Modules"  href="edition.php">édition</a></span></span></td>
+      </tr>
+   </table>
+   <p>&nbsp;</p>
+   <div class="titre"></div>
+   <p><br />
+ </p>
+</div>
+
+
+<!-------------------------------------------------------->
+
+
+  <!--PARTIE II - EXPERIENCES -->
+  
+  
+  
+  
+  <div class="sidebar2"></div>
+ 
+  
+  
+<!-- end .container --></div>
+</body>
+</html>
---
+++ _MASTER/logiciel_bck.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+
+<meta name="DESCRIPTION" content="IPX / Internet eXchange Point"/>
+<meta name="description" content="IPX / Internet eXchange Point" />
+<meta name="keyword" content="IPX, Internet eXchange Point, Ecole, Edition, Logiciel, Plateforme, Echange, Partage, Numérique, Art, Lyon, Rhone-Alpes  "/>
+<meta name="keywords" content="IPX, Internet eXchange Point, Ecole, Edition, Logiciel, Plateforme, Echange, Partage, Numérique, Art, Lyon, Rhone-Alpes"/>
+<meta name="SUBJECT" content="IPX, Internet eXchange Point, Ecole, Edition, Logiciel, Plateforme, Echange, Partage, Numérique, Art, Lyon, Rhone-Alpes "/>
+<meta name="RATING" content="IPX, Internet eXchange Point, Ecole, Edition, Logiciel, Plateforme, Echange, Partage, Numérique, Art, Lyon, Rhone-Alpes"/>
+
+<meta name="EDITORS" content="ipx-lab.com"/>
+
+<meta name="REVISIT-AFTER" content="10 DAYS"/>
+<meta name="ROBOTS" content="All"/>
+
+
+
+
+<title>LOGICIEL / Internet eXchange Point</title>
+
+
+<!-- 
+
+.....__              
+	|__|________  ___
+	|  \____ \  \/  /
+	|  |  |_> >    < 
+	|__|   __/__/\_ \
+	   |__|        \/
+
+ -->
+
+<link href="css/ipx.css" rel="stylesheet" type="text/css" />
+
+
+
+
+<style type="text/css">
+body {
+
+	color: #333;
+}
+
+
+a:link {
+	color: #FFF;
+	text-decoration: none;
+}
+a:visited {
+	color: #FFF;
+	text-decoration: none;
+}
+a:hover {
+	color: #FFF;
+	text-decoration: none;
+}
+a:active {
+	text-decoration: none;
+}
+
+
+.Bouton_IPX {
+	background-color:#0CC;
+	-moz-border-radius:42px;
+	-webkit-border-radius:42px;
+	border-radius:42px;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Trebuchet MS;
+	font-size:14px;
+	padding:13px 10px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #c9c5c9;
+}
+.Bouton_IPX:hover {
+	background-color:#dedade;
+}
+.Bouton_IPX:active {
+	position:relative;
+	top:1px;
+}
+.Bouton_Modules {
+	background-color:#0CC;
+	-moz-border-radius:42px;
+	-webkit-border-radius:42px;
+	border-radius:42px;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Trebuchet MS;
+	font-size:12px;
+	padding:18px 6px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #c9c5c9;
+}
+.Bouton_Modules:hover {
+	background-color:#dedade;
+}
+.Bouton_Modules:active {
+	position:relative;
+	top:1px;
+}
+
+.lienlogiciel{
+	color:#0CC;
+}
+
+
+.container .content table {
+	text-align: center;
+}
+
+
+
+
+
+</style>
+</head>
+
+<body>
+
+<div class="container">
+  <div class="sidebar1"><br /> <br /> <br /> 
+    <table width="100%" border="0" cellspacing="0">
+  <tr>
+    <th scope="col"><a class="Bouton_IPX"  href="../index.html">IPX</a></th>
+    <th scope="col"><a href="contact.php"><img src="img/logo.png" style="opacity: 0.4;" width="40%"></a></th>
+  </tr>
+</table>
+
+  </div>
+ 
+
+
+
+
+<div class="content">
+
+
+
+
+<!-- PARTIE I - EDUCATION -->
+
+
+<!-- école n°1-->
+
+<p><br />
+  </p>
+   <div class="titre"></div>
+   <p><br />
+    <!--FIN //////// PARTIE I - EDUCATION --></p>
+   <div class="titre"><span class="Titre_partie"></span></div>
+   <p>&nbsp;</p>
+   <div class="titre">
+     <!--  		@page { margin: 2cm }  		P { margin-bottom: 0.21cm }  	-->
+     <BODY DIR="LTR">
+     <p align="center" style="font-size:2.5em; color:#0CC;">LOGICIEL<br />
+     </p>
+<p align="center"><i>Internet eXchange Point</i></p>
+     <p align="center"><br />
+       Présentation<br />
+       Cette partie tend à proposer une formation ou une sensibilisation à de nouvelles branches de métiers liés
+       aux usages et aux pratiques du numérique. Nous tenterons de cartographier les différents éléments constitutifs de
+       la chaîne de production (Capture de l'information, traitement des données et restitutions des données). Nous mettrons
+       en place des stratégies qui piloteront nos sessions de travail en groupe afin d'optimiser les compétences de chacun,
+       de favoriser la mixité sociale et professionnelle et de mettre en place un travail situé dans une évolution créative et
+      durable au sein d'une équipe, d'un groupe ou du collectivité. </p>
+     <p align="center"> Cette ligne de recherche s'inscrit dans le courant des Software Studies, des Humanité digital et de l'archéologie des<br />
+       médias.<br />
+       À travers le développement d'une plate-forme appelé IP-F, nous tenterons de mettre en place une struc-<br />
+       ture qui convienne à nos besoins et qui pourra dans un second temps s'adapter à l'évolution des designs et des<br />
+       nouveautés.<br />
+       Composition de la recherche<br />
+       Nous œuvrons à la mise en place d'un framework simplifié, adapté, personnalisé à la structure et à l'identité<br />
+       d'une entreprise. Il s'agit d'orienter et de trouver un modèle propre à l'identité de chaque projet et d'adapter les<br />
+       outils en fonction du projet et de ses caractéristique. Il s'agit d'offrir à l'utilisateur la possibilité d'augmenter sa prise<br />
+       de décision, d'agrémenter le nombre de possibles et d'enrichir ses choix. De plus, notre postulat est de sensibiliser<br />
+       le public aux facettes éthiques et juridiques du Web, pour l'amener à se positionner, à faire des choix en harmonie<br />
+       avec ses besoins quotidien et son développement personnel. Nous souhaitons mettre en place un espace qui favorise<br />
+       et tisse du lien entre différents corps de métiers. Ce projet s'adapte à tout type de client, professionnel, amateur,<br />
+       jeune travailleur, retraité, cadre, chef d'entreprise ou encore demandeur d'emploi.<br />
+       Objectif<br />
+       1 – Service d'accompagnement, de conseil et d'expertise à la conception de systèmes d'informations<br />
+       2 – Service de fabrication adapté et sur mesure de sites (ou application) Web.<br />
+       Développement de logiciels, nouveaux outils, d'autres systèmes de fonctionnalité pour aller vers des pratiques et<br />
+       des usages éthique, open-source et le plus écologique possible<br />
+       L'accompagnement d'une idée au service d'un projet professionnel.<br />
+       3- Mise en place de méthode collective par une expérimentation et une création méthodologique de travail propre<br />
+       aux membres d'IPX. Ce processus aura pour but de construire différentes trames de travail ou la mise en place de<br />
+       framework (par exemple sous forme de tutoriaux) utile pour les ateliers. Une grande part sera donnée à la sensibili-<br />
+       sation aux langages de programmation afin de proposer aux utilisateurs des système de navigation appropriés. C'est<br />
+       un support sous forme de partition qui porte d'avantage l'objet de la discussion plutôt que le sujet de la discussion.<br />
+       4- Création d'un centre de Ressources<br />
+       5- Situer et appréhender à tout les niveaux le fonctionnement des réseaux et la diffusion de l'information : com-<br />
+       ment fonctionne le référencement, le page Ranking, des sites etc…<br />
+       Par le prisme de ce dispositif, nous tendons à réduire, voir supprimer, le nombre grandissant d'interpréta-<br />
+       tion, par conséquent de filtre, permettant non seulement d'accéder à la libre circulation des information, mais aussi<br />
+       d'y participer. Nous tenterons de réduire le langage exprimé au sentiment intime les plus simples et authentique.<br />
+       Perspectives<br />
+       a) Conservation :<br />
+       La conservation, l'hébergement relatif au lieu de stockage. Question relative à l'archéologie des médias et la ques-<br />
+       tion de la conservation : rapport à l'hébergement des données / au serveur / où sont stockées les données ? Le for-<br />
+       mat des base de données / La traçabilité, du filtrage, de la fiabilité, de la protection des données.<br />
+       b) Architecture de l'information :<br />
+       À l'organisation des documents de travail. L'arborescence des fichiers / architecture [Rapport entre architecture et<br />
+       circulation du flux des données] : ce que l'utilisateur ne voit pas = statique. L'entre-deux / code, langage, flux =<br />
+       dynamique<br />
+       c) Interface :<br />
+       Mise en page, règle typographique, colorimétrie, responsive design, UX, Navigation, norme et standard, syntaxe,<br />
+       Html et Css<br />
+       Ouverture<br />
+       La base de ressource associé au module IP:S a pour vocation d'être utilisé comme une boîte à outil au service des<br />
+       différents projets. La forme de l'outil prend la forme d'un CMS ou d'un centre de ressource accessible et libre de<br />
+       droit accessible sur notre site internet. Elle accumule et organise un ensemble d'échantillons d'expérimentation,<br />
+       fragment de code à ré-agencer, de discussion, d'exemples concrets, tutoriels, etc L'objectif est l'ouverture d'un lieu<br />
+       atelier qui permettra de réaliser cette aventure à une échelle humaine et de radical.<br /><br />
+     </p>
+    
+    
+     <p><br /><br /><br />
+</p>
+</div>
+   <p>&nbsp;</p>
+  <table width="100%" border="0">
+     <tr>
+       <td><span class="titre"><span class="Titre_partie"><a class="Bouton_Modules" href="ecole.php">&nbsp;école&nbsp;</a></span></span></td>
+       <td><span class="titre"><span class="Titre_partie"><a class="Bouton_Modules"  href="logiciel.php">logiciel</a></span></span></td>
+       <td><span class="titre"><span class="Titre_partie"><a class="Bouton_Modules"  href="edition.php">édition</a></span></span></td>
+      </tr>
+   </table>
+   <p>&nbsp;</p>
+   <div class="titre"></div>
+   <p><br />
+ </p>
+</div>
+
+
+<!-------------------------------------------------------->
+
+
+  <!--PARTIE II - EXPERIENCES -->
+  
+  
+  
+  
+  <div class="sidebar2"></div>
+ 
+  
+  
+<!-- end .container --></div>
+</body>
+</html>
