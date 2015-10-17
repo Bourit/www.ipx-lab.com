62fd2cc68f30744debc0f582435a28012a18dcce
florentlagrangeipx <contact@florentlagrange.eu>
2015-10-17 11:15:07 +0200

    Introduction [MASTER]
    Introduction [MASTER]

---
+++ _MASTER/logiciel.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+
+<meta name="DESCRIPTION" content="IPX / Internet eXchange Point"/>
+<meta name="description" content="IPX / Internet eXchange Point" />
+<meta name="keyword" content="IPX, Internet eXchange Point, Ecole, Edition, Logiciel, Plateforme, Echange, Partage, Numérique, Art, Lyon, Rhone-Alpes  "/>
+<meta name="keywords" content="IPX, Internet eXchange Point, Ecole, Edition, Logiciel, Plateforme, Echange, Partage, Numérique, Art, Lyon, Rhone-Alpes"/>
+<meta name="SUBJECT" content="IPX, Internet eXchange Point, Ecole, Edition, Logiciel, Plateforme, Echange, Partage, Numérique, Art, Lyon, Rhone-Alpes "/>
+<meta name="RATING" content="IPX, Internet eXchange Point, Ecole, Edition, Logiciel, Plateforme, Echange, Partage, Numérique, Art, Lyon, Rhone-Alpes"/>
+
+<meta name="EDITORS" content="ipx-lab.com"/>
+
+<meta name="REVISIT-AFTER" content="10 DAYS"/>
+<meta name="ROBOTS" content="All"/>
+
+
+
+
+<title>LOGICIEL / Internet eXchange Point</title>
+
+
+<!-- 
+
+.....__              
+	|__|________  ___
+	|  \____ \  \/  /
+	|  |  |_> >    < 
+	|__|   __/__/\_ \
+	   |__|        \/
+
+ -->
+
+<link href="css/ipx.css" rel="stylesheet" type="text/css" />
+
+
+
+
+<style type="text/css">
+body {
+
+	color: #333;
+}
+
+
+a:link {
+	color: #FFF;
+	text-decoration: none;
+}
+a:visited {
+	color: #FFF;
+	text-decoration: none;
+}
+a:hover {
+	color: #FFF;
+	text-decoration: none;
+}
+a:active {
+	text-decoration: none;
+}
+
+
+.Bouton_IPX {
+	background-color:#0CC;
+	-moz-border-radius:42px;
+	-webkit-border-radius:42px;
+	border-radius:42px;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Trebuchet MS;
+	font-size:14px;
+	padding:13px 10px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #c9c5c9;
+}
+.Bouton_IPX:hover {
+	background-color:#dedade;
+}
+.Bouton_IPX:active {
+	position:relative;
+	top:1px;
+}
+.Bouton_Modules {
+	background-color:#0CC;
+	-moz-border-radius:42px;
+	-webkit-border-radius:42px;
+	border-radius:42px;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Trebuchet MS;
+	font-size:12px;
+	padding:18px 6px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #c9c5c9;
+}
+.Bouton_Modules:hover {
+	background-color:#dedade;
+}
+.Bouton_Modules:active {
+	position:relative;
+	top:1px;
+}
+
+.lienlogiciel{
+	color:#0CC;
+}
+
+
+.container .content table {
+	text-align: center;
+}
+
+
+
+
+
+</style>
+</head>
+
+<body dir="ltr">
+
+<div class="container">
+  <div class="sidebar1"><br /> <br /> <br /> 
+    <table width="100%" border="0" cellspacing="0">
+  <tr>
+    <th scope="col"><a class="Bouton_IPX"  href="../index.html">IPX</a></th>
+    <th scope="col"><a href="contact.php"><img src="img/logo.png" style="opacity: 0.4;" width="40%"></a></th>
+  </tr>
+</table>
+
+  </div>
+ 
+
+
+
+
+<div class="content">
+
+
+
+
+<!-- PARTIE I - EDUCATION -->
+
+
+<!-- école n°1-->
+
+<p><br />
+  </p>
+   <div class="titre"></div>
+   <p><br />
+    <!--FIN //////// PARTIE I - EDUCATION --></p>
+   <div class="titre"><span class="Titre_partie"></span></div>
+   <p>&nbsp;</p>
+   <div class="titre">
+     <div class="titre">
+     <!--  		@page { margin: 2cm }  		P { margin-bottom: 0.21cm }  	-->
+      <p align="center" style="font-size:2.5em; color:#0CC;">LOGICIEL<br />
+     </p>
+     <p align="center"><i>Internet eXchange Point</i></p>
+     <p align="center"><br />
+     Texte en cours de rédaction<br />
+       <br />
+       <br />
+     </p>
+     <p><em class="sous_titre_edition">Présentation</em><br />
+     </p>
+     <p>Cette partie tend à proposer une formation ou une sensibilisation à de nouvelles branches de métiers liés
+aux usages et aux pratiques du numérique. Nous tenterons de cartographier les différents éléments constitutifs de
+la chaîne de production (Capture de l'information, traitement des données et restitutions des données). Nous mettrons
+en place des stratégies qui piloteront nos sessions de travail en groupe afin d'optimiser les compétences de chacun,
+de favoriser la mixité sociale et professionnelle et de mettre en place un travail situé dans une évolution créative et
+durable au sein d'une équipe, d'un groupe ou du collectivité.<br />
+       <br />
+       Cette ligne de recherche s'inscrit dans le courant des Software Studies, des Humanité digital et de l'archéologie des
+       médias.</p>
+     <p><br />
+       À travers le développement d'une plate-forme appelé IP-F, nous tenterons de mettre en place une structure qui convienne à nos besoins et qui pourra dans un second temps s'adapter à l'évolution des designs et des<br />
+       nouveautés..<br />
+     </p>
+     <p><em class="sous_titre_edition">Composition de la recherche</em><br />
+   </p>
+     <p>Nous œuvrons à la mise en place d'un framework simplifié, adapté, personnalisé à la structure et à l'identité
+d'une entreprise. Il s'agit d'orienter et de trouver un modèle propre à l'identité de chaque projet et d'adapter les
+outils en fonction du projet et de ses caractéristique. Il s'agit d'offrir à l'utilisateur la possibilité d'augmenter sa prise
+de décision, d'agrémenter le nombre de possibles et d'enrichir ses choix. De plus, notre postulat est de sensibiliser
+le public aux facettes éthiques et juridiques du Web, pour l'amener à se positionner, à faire des choix en harmonie
+avec ses besoins quotidien et son développement personnel. Nous souhaitons mettre en place un espace qui favorise
+et tisse du lien entre différents corps de métiers. Ce projet s'adapte à tout type de client, professionnel, amateur,
+jeune travailleur, retraité, cadre, chef d'entreprise ou encore demandeur d'emploi.</p>
+     <p>1 – Service d'accompagnement, de conseil et d'expertise à la conception de systèmes d'informations.</p>
+     <p>2 – Service de fabrication adapté et sur mesure de sites (ou application) Web.<br />
+       Développement de logiciels, nouveaux outils, d'autres systèmes de fonctionnalité pour aller vers des pratiques et
+       des usages éthique, open-source et le plus écologique possible<br />
+       L'accompagnement d'une idée au service d'un projet professionnel.</p>
+     <p>3- Mise en place de méthode collective par une expérimentation et une création méthodologique de travail propre
+       aux membres d'IPX. Ce processus aura pour but de construire différentes trames de travail ou la mise en place de
+       framework (par exemple sous forme de tutoriaux) utile pour les ateliers. Une grande part sera donnée à la sensibilisation aux langages de programmation afin de proposer aux utilisateurs des système de navigation appropriés. C'est
+       un support sous forme de partition qui porte d'avantage l'objet de la discussion plutôt que le sujet de la discussion.</p>
+     <p>4- Création d'un centre de Ressources</p>
+     <p>5- Situer et appréhender à tout les niveaux le fonctionnement des réseaux et la diffusion de l'information : comment fonctionne le référencement, le page Ranking, des sites etc…</p>
+     <p>Par le prisme de ce dispositif, nous tendons à réduire, voir supprimer, le nombre grandissant d'interprétation, par conséquent de filtre, permettant non seulement d'accéder à la libre circulation des information, mais aussi
+       d'y participer. Nous tenterons de réduire le langage exprimé au sentiment intime les plus simples et authentique<br />
+       <br />
+ </p>
+     <p><em class="sous_titre_edition">Ouvertures</em><br />
+ </p>
+     <p>La base de ressource associé au module IP:S a pour vocation d'être utilisé comme une boîte à outil au service des
+       différents projets. La forme de l'outil prend la forme d'un CMS ou d'un centre de ressource accessible et libre de
+       droit accessible sur notre site internet. Elle accumule et organise un ensemble d'échantillons d'expérimentation,
+       fragment de code à ré-agencer, de discussion, d'exemples concrets, tutoriels, etc L'objectif est l'ouverture <br />
+       <br />
+       <br />
+     </p>
+     </div>
+   </div>
+   <p>&nbsp;</p>
+  <table width="100%" border="0">
+     <tr>
+       <td><span class="titre"><span class="Titre_partie"><a class="Bouton_Modules" href="ecole.php">&nbsp;école&nbsp;</a></span></span></td>
+       <td><span class="titre"><span class="Titre_partie"><a class="Bouton_Modules"  href="logiciel.php">logiciel</a></span></span></td>
+       <td><span class="titre"><span class="Titre_partie"><a class="Bouton_Modules"  href="edition.php">édition</a></span></span></td>
+      </tr>
+   </table>
+   <p>&nbsp;</p>
+   <div class="titre"></div>
+   <p><br />
+ </p>
+</div>
+
+
+<!-------------------------------------------------------->
+
+
+  <!--PARTIE II - EXPERIENCES -->
+  
+  
+  
+  
+  <div class="sidebar2"></div>
+ 
+  
+  
+<!-- end .container --></div>
+</body>
+</html>
